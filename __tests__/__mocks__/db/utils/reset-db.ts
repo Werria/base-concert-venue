3a8090fa0972f4be1ce14a672170c5fb43d7544f
Weria Heidary <verya.heydari@gmail.com>
2022-11-24 01:20:22 +0330

    set up reseting db before any cypress test

--- __tests__/__mocks__/db/utils/reset-db.ts
+++ __tests__/__mocks__/db/utils/reset-db.ts
@@ -2,7 +2,7 @@ import { readFakeData } from "@/__tests__/__mocks__/fakeData";
 import { filenames, writeJSONToFile } from "@/lib/db/db-utils";
 
 export const resetDB = async () => {
-  const safeToReset = process.env.NODE_ENV === "test";
+  const safeToReset = process.env.NODE_ENV === "test" || process.env.CYPRESS;
   if (!safeToReset) {
     console.log("WARNING: database reset unavailable outside test env");
     return;
