40aa60a500a90ee8f57e31f4e0a4d5691341b084
Weria Heidary <verya.heydari@gmail.com>
2022-11-24 01:04:05 +0330

    add cypress static route tests

--- cypress/e2e/routes.cy.js
+++ cypress/e2e/routes.cy.js
@@ -3,3 +3,11 @@ it("displays correct heading when navigating to show route", () => {
   cy.findByRole("button", { name: /shows/i }).click();
   cy.findByRole("heading", { name: /upcoming shows/i }).should("exist");
 });
+
+it("checks the bands route", () => {
+  cy.visit("/");
+  cy.findByRole("button", { name: /bands/i }).click();
+  cy.findByRole("heading", { name: /Our Illustrious Performers/i }).should(
+    "exist"
+  );
+});
