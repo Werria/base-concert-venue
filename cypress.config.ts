7bd24d4ca6f6642abadf9118ef586d4b7c15db75
Weria Heidary <verya.heydari@gmail.com>
2022-11-25 10:56:08 +0330

    test band route that is present after build time

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "cypress";
+import { addBand } from "./lib/features/bands/queries";
 import { resetDB } from "./__tests__/__mocks__/db/utils/reset-db";
 
 export default defineConfig({
@@ -7,6 +8,7 @@ export default defineConfig({
       // implement node event listeners here
       on("task", {
         "db:reset": () => resetDB().then(() => null),
+        addBand: (newBand) => addBand(newBand).then(() => null),
       });
     },
   },
--- cypress/e2e/routes.cy.js
+++ cypress/e2e/routes.cy.js
@@ -1,3 +1,6 @@
+import { generateNewBand } from "@/__tests__/__mocks__/fakeData/newBand";
+import { generateRandomId } from "@/lib/features/reservations/utils";
+
 it("displays correct heading when navigating to show route", () => {
   cy.visit("/");
   cy.findByRole("button", { name: /shows/i }).click();
@@ -20,4 +23,11 @@ it("displays correct band name for band route that existed at build time", () =>
 it("displays correct message for unkhnown band id route", () => {
   cy.task("db:reset").visit("/bands/12345");
   cy.findByRole("heading", { name: /Error: band not found/i }).should("exist");
+});
+
+it("displays band name that was not present at build time", () => {
+  const bandId = generateRandomId();
+  const newBand = generateNewBand(bandId);
+  cy.task("db:reset").task("addBand", newBand).visit(`/bands/${bandId}`);
+  cy.findByRole("heading", { name: /Avalanche of Cheese/i }).should("exist");
 });
