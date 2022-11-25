98e9012271c3fc1a96b4742845aea40942530448
Weria Heidary <verya.heydari@gmail.com>
2022-11-25 10:03:21 +0330

    test unkhown band id route

--- cypress/e2e/routes.cy.js
+++ cypress/e2e/routes.cy.js
@@ -16,3 +16,8 @@ it("displays correct band name for band route that existed at build time", () =>
   cy.task("db:reset").visit("/bands/1");
   cy.findByRole("heading", { name: /Shamrock Pete/i }).should("exist");
 });
+
+it("displays correct message for unkhnown band id route", () => {
+  cy.task("db:reset").visit("/bands/12345");
+  cy.findByRole("heading", { name: /Error: band not found/i }).should("exist");
+});
